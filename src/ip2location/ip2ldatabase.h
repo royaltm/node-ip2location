6e8f0f11bcc134651232fd704e11409c31f0bf48
Rafał Michalski <royaltm75@gmail.com>
2015-02-15 15:08:33 +0100

    return value info

--- src/ip2location/ip2ldatabase.h
+++ src/ip2location/ip2ldatabase.h
@@ -13,11 +13,14 @@ IP2LMemoryMapList *IP2LocationSetupMMap(FILE *filehandle, size_t dbfilesize, cha
 IP2LMemoryMapList *IP2LocationSetupShared(FILE *filehandle, size_t dbfilesize, char *shared_name);
 
 void IP2LocationRead128Buffer32LE(uint32_t *buffer, FILE *handle, uint8_t *cache, uint32_t position);
-uint32_t IP2LocationRead32(FILE *handle, uint8_t *cache, uint32_t position);
 uint8_t IP2LocationRead8(FILE *handle, uint8_t *cache, uint32_t position);
+float IP2LocationReadFloat(FILE *handle, uint8_t *cache, uint32_t position);
+uint32_t IP2LocationRead32(FILE *handle, uint8_t *cache, uint32_t position);
+/* the value returned by the following functions is not a C-string (null terminated)
+   the 1st byte indicates a string length as unsigned char
+   the string starts at 2nd byte if length > 0 */
 const char *IP2LocationReadStr(FILE *handle, uint8_t *cache, uint32_t position);
 const char *IP2LocationReadStrIndexAtOffset(FILE *handle, uint8_t *cache, uint32_t position, int offset);
-float IP2LocationReadFloat(FILE *handle, uint8_t *cache, uint32_t position);
 
 int IP2LocationDeleteSharedMap(IP2LMemoryMapList *shmnode);
 void IP2LocationDBClose(FILE *filehandle, IP2LMemoryMapList *shmnode);
